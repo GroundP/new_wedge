a183d7048b343190bb3bba91e0ad0580a10294d9
groundp <bloom0819@naver.com>
2022-07-19 01:00:56 +0900

    add static path(css, js) change icon TODO) config sidebar, table

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
+const nunjucks = require('nunjucks');
 
 const app = express();
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS  
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('port', process.env.PORT || 3000);
+// app.set('view engine', 'html');
+// nunjucks.configure('views', {
+//     express: app,
+//     watch: true,
+// });
 
 app.get('/', (req, res) => {
     //res.send('Hello, Express');
