2cbf25233a594b60a27f7922486fe8edfd8ef6f9
groundp <bloom0819@naver.com>
2022-08-01 00:20:21 +0900

    Edit Spoon configuration

--- routes/spoon.js
+++ routes/spoon.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {Spoon} = require('../models');
+const { Spoon } = require('../models');
 const router = express.Router();
 
 
@@ -8,7 +8,7 @@ router.get('/', async (req, res, next) => {
         const spoons = await Spoon.findAll({
             //order: [['createdAt', 'DESC']],
         });
-        res.render('main', {
+        res.render('spoon', {
             title: 'new_wedge',
             spoonValues: spoons,
         });
@@ -17,5 +17,60 @@ router.get('/', async (req, res, next) => {
         next(err);
     }
 });
+
+router.post('/', async (req, res, next) => {
+    const {
+        mkd_log_level,
+        mkd_log_price,
+        mkd_spread_count,
+        mkd_spread_ratio,
+        mkd_spread_interval,
+        mkd_max_spread_factor,
+        mkd_min_spread_factor,
+        service_host,
+        service_port
+    } = req.body;
+
+    try {
+        const exSpoon = await Spoon.findOne({ where: { cd: 1 } });
+        if (!exSpoon) {
+            return res.redirect('/add?error=notexist');
+        }
+        await Spoon.update({
+            mkd_log_level,
+            mkd_log_price,
+            mkd_spread_count,
+            mkd_spread_ratio,
+            mkd_spread_interval,
+            mkd_max_spread_factor,
+            mkd_min_spread_factor,
+            service_host,
+            service_port
+        }, { where: { cd: 1 } });
+
+        return res.redirect('/spoon');
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+})
+
+
+router.get('/edit', async (req, res, next) => {
+    try {
+        const spoons = await Spoon.findAll({
+        });
+
+        res.render('spoon', {
+            title: 'new_wedge',
+            spoonValues: spoons,
+            edit_flag: true,
+        });
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+
 
 module.exports = router;
